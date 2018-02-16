9e7566d5589cf73f01fcb4642744d0ff81aa9473
Vladimir Kocis Tubic <vktubic@elsys-eastern.rs>
2018-02-16 10:11:06 +0100

    Finally finished transfer

---
+++ VladimirKT/ViewModels/MovieCustomerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VladimirKT.Models;
+
+namespace VladimirKT.ViewModels
+{
+	public class MovieCustomerViewModel
+	{
+		public Customer Customer { get; set; }
+		public Movie Movie { get; set; }
+		public List<string> MovieNames { get; set; } 
+	}
+}
---
+++ VladimirKT/ViewModels/MovieViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using VladimirKT.Models;
+
+namespace VladimirKT.ViewModels
+{
+	public class MovieViewModel
+	{
+		public IEnumerable<Movie> Movies { get; set; }
+		public Movie Movie { get; set; }
+
+		[Required]
+		[Display(Name = "Yours Credit card number")]
+		[Range(1000000, 9999999, ErrorMessage = "Please enter 7 digits number between 1000000 and 9999999")]
+		public int CardNumber { get; set; }
+	}
+}
